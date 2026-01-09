98abfc78e06fccee6e993c29da3bbacf76ecf43a
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-09 15:50:01 +0100

    third fuction snack filter array for first letter

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,30 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function firstLetter(arrayStrings, letter) {
+  const result = [];
+
+  for (let i = 0; i < arrayStrings.length; i++) {
+    // console.log(arrayStrings[i].toLowerCase());
+    // console.log(arrayStrings[i].toLowerCase()[0]);
+
+    // console.log(letter.toLowerCase());
+    if (arrayStrings[i].toLowerCase()[0] === letter.toLowerCase()) {
+      result.push(arrayStrings[i]);
+    }
+  }
+
+  return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const result = firstLetter(names, "a");
+console.log(result);
+// const result2 = firstLetter(names, "m");
+// console.log(result2);
+// const result3 = firstLetter(names, "l");
+// console.log(result3);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
